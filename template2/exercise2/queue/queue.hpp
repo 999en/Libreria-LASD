b5bc1371efec5e1d9457cc838da20bec921706e9
Praise32 <marianosommella02@gmail.com>
2024-05-03 17:35:33 +0200

    stack.hpp e queue.hpp

--- template2/exercise2/queue/queue.hpp
+++ template2/exercise2/queue/queue.hpp
@@ -12,49 +12,45 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-template <typename Data>
-class Queue {
-  // Must extend ClearableContainer
+    template <typename Data>
+    class Queue : public virtual ClearableContainer{
+        // Must extend ClearableContainer
 
-private:
+    private:
 
-  // ...
+    protected:
 
-protected:
+    public:
 
-  // ...
+        // Destructor
+        virtual ~Queue() = default;
 
-public:
+        /* ************************************************************************ */
 
-  // Destructor
-  // ~Queue() specifiers
+        // Copy assignment
+        Queue& operator=(const Queue&) = delete; // Copy assignment of abstract types is not possible.
 
-  /* ************************************************************************ */
+        // Move assignment
+        Queue& operator=(Queue&&) noexcept = delete; // Move assignment of abstract types is not possible.
 
-  // Copy assignment
-  // type operator=(argument); // Copy assignment of abstract types is not possible.
+        /* ************************************************************************ */
 
-  // Move assignment
-  // type operator=(argument); // Move assignment of abstract types is not possible.
+        // Comparison operators
+        bool operator==(const Queue&) const noexcept = delete; // Comparison of abstract types is not possible.
+        bool operator!=(const Queue&) const noexcept = delete; // Comparison of abstract types is not possible.
 
-  /* ************************************************************************ */
+        /* ************************************************************************ */
 
-  // Comparison operators
-  // type operator==(argument) specifiers; // Comparison of abstract types is not possible.
-  // type operator!=(argument) specifiers; // Comparison of abstract types is not possible.
+        // Specific member functions
 
-  /* ************************************************************************ */
+        virtual const Data& Head() const = 0; // (non-mutable version; concrete function must throw std::length_error when empty)
+        virtual Data& Head() = 0; // (mutable version; concrete function must throw std::length_error when empty)
+        virtual void Dequeue() = 0; // (concrete function must throw std::length_error when empty)
+        virtual Data HeadNDequeue() = 0; // (concrete function must throw std::length_error when empty)
+        virtual void Enqueue(const Data&) = 0; // Copy of the value
+        virtual void Enqueue(Data&&) = 0; // Move of the value
 
-  // Specific member functions
-
-  // type Head() specifiers; // (non-mutable version; concrete function must throw std::length_error when empty)
-  // type Head() specifiers; // (mutable version; concrete function must throw std::length_error when empty)
-  // type Dequeue() specifiers; // (concrete function must throw std::length_error when empty)
-  // type HeadNDequeue() specifiers; // (concrete function must throw std::length_error when empty)
-  // type Enqueue(argument) specifiers; // Copy of the value
-  // type Enqueue(argument) specifiers; // Move of the value
-
-};
+    };
 
 /* ************************************************************************** */
 
