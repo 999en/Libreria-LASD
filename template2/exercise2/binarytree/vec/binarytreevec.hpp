c7112a1ba1c58a59b72dc876cc852720c112eaee
999en <125801405+999en@users.noreply.github.com>
2024-05-06 16:30:36 +0200

    Update binarytreevec.hpp

--- template2/exercise2/binarytree/vec/binarytreevec.hpp
+++ template2/exercise2/binarytree/vec/binarytreevec.hpp
@@ -96,7 +96,7 @@ using BinaryTree<Data>::PostOrderMap;
   /* ************************************************************************ */
 
   // Specific constructors
-  // BinaryTreeVec(argument) specifiers; // A binary tree obtained from a TraversableContainer
+  BinaryTreeVec(const TraversableContainer<Data>& right); // A binary tree obtained from a TraversableContainer
   BinaryTreeVec(const MappableContainer<Data>& right);   // A binary tree obtained from a MappableContainer
   /* ************************************************************************ */
 
