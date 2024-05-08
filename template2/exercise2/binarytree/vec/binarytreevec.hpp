1004d67fc34182fc0d9ba11acaaf5312ca885437
999en <125801405+999en@users.noreply.github.com>
2024-05-08 11:43:13 +0200

    Update binarytreevec.hpp
    sistemato 2

--- template2/exercise2/binarytree/vec/binarytreevec.hpp
+++ template2/exercise2/binarytree/vec/binarytreevec.hpp
@@ -96,7 +96,7 @@ using BinaryTree<Data>::PostOrderMap;
   /* ************************************************************************ */
 
   // Specific constructors
-  BinaryTreeVec(TraversableContainer<Data>&& right); // A binary tree obtained from a TraversableContainer
+  BinaryTreeVec(TraversableContainer<Data>&& right) noexcept; // A binary tree obtained from a TraversableContainer
   BinaryTreeVec(const MappableContainer<Data>& right);   // A binary tree obtained from a MappableContainer
   /* ************************************************************************ */
 
